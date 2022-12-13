7fd8792d25be0dd8ded948df455ff3d6ae875b41
Pyglik <pyglik@o2.pl>
2022-12-13 14:13:25 +0100

    porządki

--- get_battery_state.m
+++
@@ -1,5 +0,0 @@
-function battery_state = get_battery_state(battery_state_sub)
-    battery_state = receive(battery_state_sub, 5).Percent; % czas oczekiwania [s]
-end
-
-% procent naładowania baterii
--- get_wifi_state.m
+++
@@ -1,5 +0,0 @@
-function wifi_state = get_wifi_state(wifi_state_sub)
-    wifi_state = receive(wifi_state_sub, 5).Rssi; % czas oczekiwania [s]
-end
-
-% siła sygnału Wi-Fi w dB
